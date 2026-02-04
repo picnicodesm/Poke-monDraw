210831d1535f2dfb0e73f5098308876eb27a80f6
Sangmin Kim <offiksm0903@naver.com>
2026-02-04 14:01:09 +0900

    Initial Commit

---
+++ PokémonDraw/Poke_monDrawApp.swift
@@ -0,0 +1,17 @@
+//
+//  Poke_monDrawApp.swift
+//  PokémonDraw
+//
+//  Created by picnic on 2/4/26.
+//
+
+import SwiftUI
+
+@main
+struct Poke_monDrawApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
